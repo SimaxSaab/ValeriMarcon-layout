b4bee3f4c47e4914a27fe19ceb9fcd456d56e84e
SimaxSaab <minorea@yandex.ru>
2020-12-08 12:45:37 +0200

    Deploy to netlify

---
+++ ValeriMarcon/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // аля $(document).ready
+  var videos = document.getElementsByClassName('video'); // получаем все видосы с класосм
+
+  videos = [].slice.call(videos); // преобразуем в массив
+
+  videos.forEach(function (item) {
+    // перебираем массив и навешиваем обработчики
+    var media = item;
+
+    media.onclick = function () {
+      media.play(); // запускаем проигрывание
+
+      media.controls = true; // добавляем контролы
+    }; // media.addEventListener('ended', function() {
+    //   media.controls = false; // видео просмотрено, убираем контролы
+    // });
+
+  });
+});
